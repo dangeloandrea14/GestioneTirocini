8234931f04eecb468ff31097a7613ec54c80c9e0
dangeloandrea14 <37885084+dangeloandrea14@users.noreply.github.com>
2019-08-04 13:17:41 +0200

    Push che ancora non funziona

--- tirocini-nb/src/main/java/com/univaq/tirocini/controller/Profile.java
+++ tirocini-nb/src/main/java/com/univaq/tirocini/controller/Profile.java
@@ -5,11 +5,16 @@
  */
 package com.univaq.tirocini.controller;
 
+import com.univaq.tirocini.data.DAO.TirocinioDataLayer;
+import com.univaq.tirocini.data.model.Studente;
+import com.univaq.tirocini.framework.data.DataException;
 import com.univaq.tirocini.framework.result.FailureResult;
 import com.univaq.tirocini.framework.result.TemplateManagerException;
 import com.univaq.tirocini.framework.result.TemplateResult;
 import com.univaq.tirocini.framework.security.SecurityLayer;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,7 +40,15 @@ public class Profile extends TirociniBaseController {
             }            
             
             TemplateResult res = new TemplateResult(getServletContext());
-
+            
+            try {
+                request.setAttribute("tirocini", ((TirocinioDataLayer)request.getAttribute("datalayer")).getTirocinioDAO().getTirocini((Studente) request.getSession().getAttribute("studente")));
+            
+            } catch (DataException ex) {
+            request.setAttribute("message", "Data access exception: " + ex.getMessage());
+            action_error(request, response);
+        }
+            
             res.activate("profile.ftl.html", request, response);
     }
 
