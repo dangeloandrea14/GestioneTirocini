1a51fd745b2c6943ce4113e92cc80d2778406dde
Andrea <Shoken@SHOKENPC.station>
2019-08-25 13:00:24 +0200

    Concludere un tirocinio con successo

---
+++ tirocini-nb/src/main/java/com/univaq/tirocini/controller/ConclusioneTirocinio.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.univaq.tirocini.controller;
+
+import com.univaq.tirocini.data.DAO.TirocinioDataLayer;
+import com.univaq.tirocini.data.model.Offerta;
+import com.univaq.tirocini.data.model.Tirocinio;
+import com.univaq.tirocini.framework.data.DataException;
+import com.univaq.tirocini.framework.result.TemplateManagerException;
+import com.univaq.tirocini.framework.result.TemplateResult;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Andrea
+ */
+public class ConclusioneTirocinio extends TirociniBaseController {
+
+    @Override
+   protected void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+       
+       try{
+    request.setAttribute("page_title", "Conclusione Tirocinio");
+    String param = request.getParameter("i");
+    
+    int tid = Integer.parseInt(param);
+   
+    Tirocinio tirocinio = (Tirocinio) ((TirocinioDataLayer)request.getAttribute("datalayer")).getTirocinioDAO().getTirocinio(tid);
+
+            
+    request.setAttribute("tirocinio",tirocinio);
+     
+     
+        TemplateResult res = new TemplateResult(getServletContext());
+
+        res.activate("conclusione.ftl.html", request, response); 
+        
+       } catch (DataException ex) {
+            request.setAttribute("message", "Data access exception: " + ex.getMessage());
+            action_error(request, response);
+        }
+
+       
+       
+    }
+
+   
+}
