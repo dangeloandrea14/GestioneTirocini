b3b9bb2e5ce889cf48bc4cbe92f4b7484dc8bc6b
dangeloandrea14 <37885084+dangeloandrea14@users.noreply.github.com>
2019-10-23 14:58:19 +0200

    push di prova

--- tirocini-nb/src/main/java/com/univaq/tirocini/controller/Admin.java
+++ tirocini-nb/src/main/java/com/univaq/tirocini/controller/Admin.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.univaq.tirocini.controller;
 
 import com.univaq.tirocini.data.DAO.AziendaDAO;
