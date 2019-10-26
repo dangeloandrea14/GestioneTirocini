4f8bd19a13ff6495336b98ff96c0343ef3256d19
Francesco Zappacosta <francesco.zappacosta96@gmail.com>
2019-10-27 00:01:35 +0200

    regchooser

--- tirocini-nb/src/main/java/com/univaq/tirocini/controller/Registration.java
+++ tirocini-nb/src/main/java/com/univaq/tirocini/controller/Registration.java
@@ -47,7 +47,7 @@ public class Registration extends TirociniBaseController {
         //il tipo di registrazione che si vuole effetturare
 
         if (p == null) {
-            res.activate("registrationChooser(versione da adattare).ftl.html", request, response);
+            res.activate("registrationChooser.ftl.html", request, response);
         } else if (p.equals("studente")) {
             res.activate("registrazioneStudente.ftl.html", request, response);
         } else if (p.equals("azienda")) {
