16cf0b8ca72ba282abb3729e773fef65a29c31c2
Francesco Zappacosta <francesco.zappacosta96@gmail.com>
2019-11-05 22:20:21 +0100

    ciao

--- tirocini-nb/src/main/java/com/univaq/tirocini/controller/DettagliAzienda.java
+++ tirocini-nb/src/main/java/com/univaq/tirocini/controller/DettagliAzienda.java
@@ -5,12 +5,16 @@
  */
 package com.univaq.tirocini.controller;
 
+import com.univaq.tirocini.data.DAO.TirocinioDataLayer;
 import com.univaq.tirocini.data.model.Azienda;
+import com.univaq.tirocini.data.model.Offerta;
+import com.univaq.tirocini.data.model.Tirocinio;
 import com.univaq.tirocini.framework.data.DataException;
 import com.univaq.tirocini.framework.result.TemplateManagerException;
 import com.univaq.tirocini.framework.result.TemplateResult;
 import com.univaq.tirocini.framework.security.SecurityLayer;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,7 +32,7 @@ public class DettagliAzienda extends TirociniBaseController {
             notFound(request, response);
             return;
         }
-
+         TirocinioDataLayer datalayer = dataLayer(request);
         Azienda azienda;
 
         azienda = dataLayer(request).getAziendaDAO().getAzienda(SecurityLayer.checkNumeric(p));
@@ -37,7 +41,39 @@ public class DettagliAzienda extends TirociniBaseController {
             notFound(request, response);
             return;
         }
-
+        
+         azienda = datalayer.getAziendaDAO().getAzienda(SecurityLayer.checkNumeric(p));
+       
+        List<com.univaq.tirocini.data.model.Valutazione> voti = datalayer.getValutazioneDAO().getValutazioni(azienda);
+        
+        int unastella=0,duestelle=0,trestelle=0,quattrostelle=0,cinquestelle=0;
+        
+        
+        for (com.univaq.tirocini.data.model.Valutazione voto : voti){
+            
+            switch(voto.getStelle()){
+                case 1: unastella++;
+                    break;
+                case 2: duestelle++;
+                    break;
+                case 3: trestelle++;
+                    break;
+                case 4: quattrostelle++;
+                    break;
+                case 5: cinquestelle++;
+                    break;
+            }
+         
+        }
+        
+       
+        
+        
+           request.setAttribute("unastella",unastella);
+        request.setAttribute("duestelle",duestelle);
+        request.setAttribute("trestelle",trestelle);
+        request.setAttribute("quattrostelle",quattrostelle);
+        request.setAttribute("cinquestelle",cinquestelle);
         request.setAttribute("page_title", "Dettagli " + azienda.getNome());
         request.setAttribute("aziendap", azienda);
         request.setAttribute("offerte", dataLayer(request).getOffertaDAO().getOfferte(azienda));
